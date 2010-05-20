82618bf20a2b743ab73284e3ce748521f978f779
Abdelkader ELKALIDI <contact@updel.com>
2010-05-20 18:21:44 +0000

    Signed-off-by: Abdelkader ELKALIDI <contact@updel.com>

--- Source/drag2crop.class.js
+++ Source/drag2crop.class.js
@@ -1,3 +1,24 @@
+/*
+---
+description: Drag picture inside a small box to get a thumbnail showing the good part of the picture.
+
+license: 
+	- MIT-style
+
+authors:
+	- Abdelkader ELKALIDI (http://updel.com)
+
+requires:
+	- core/1.2.4:   '*'
+	- more/1.2.4: 'Drag.*'
+
+provides:
+	- Drag2Crop
+  
+version:
+	- 0.31
+...
+*/
 var Drag2Crop = new Class({
     Extends: Drag,
     Implements:[Options,Events],
@@ -22,11 +43,14 @@ var Drag2Crop = new Class({
 	},
     coordinates	: function(){
 		this.getCoordinates	=	this.relative.getScroll();
-        this.left  			=   this.getCoordinates.x;
-        this.top    		=   this.getCoordinates.y;
+		this.getSize		=	this.relative.getSize();
+        this.width  		=   this.relative.getSize().x.toInt();
+        this.height  		=   this.relative.getSize().y.toInt();
+        this.left  			=   this.getCoordinates.x.toInt();
+        this.top    		=   this.getCoordinates.y.toInt();
     },
 	complete    :    function(){
 		this.coordinates();
-		this.fireEvent("done",[this.top,this.left]);
+		this.fireEvent("done",[this.top,this.left,this.width,this.height]);
 	}
 });
