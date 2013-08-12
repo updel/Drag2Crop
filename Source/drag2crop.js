99be543a555b8892a2b735cb337bc519d52ccb0e
Rudolph Sand <rudolph.sand@gmail.com>
2013-08-12 18:37:26 +0200

    Updated to mootools 1.4.5

--- Source/drag2crop.js
+++ Source/drag2crop.js
@@ -7,52 +7,54 @@ license:
 
 authors:
   Abdelkader ELKALIDI (http://updel.com)
+  Updateded to mootools 1.4.5 by Rudolph Sand
 
 requires:
-  core/1.2.4:   '*'
-  more/1.2.4: 'Drag.*'
+  core/1.4.5:   '*'
+  more/1.4.5: 'Drag.*'
 
 provides:
   Drag2Crop
   
 version:
-  0.4
+  0.5
 ...
 */
 var Drag2Crop = new Class({
     Extends: Drag,
     Implements:[Options,Events],
-    initialize: function(picture, options) {
-        this.setOptions({
+    options: {
             relative    :    'relative',
             modifiers   :    { x: 'scrollLeft', y: 'scrollTop' },
             style       :    false,
             invert      :    true,
-            top  		:    0,
-            left		:    0,
-            onComplete  :    this.complete.bind(this),
+            top         :    0,
+            left        :    0,
+            onComplete  :    function() { this.complete(); },
             debug       :    false
-        }, options);
-		this.top        =     this.options.top.toInt();
+    },
+    initialize: function(picture, options) {
+        this.setOptions(options);
+        this.top        =     this.options.top.toInt();
         this.left       =     this.options.left.toInt();
-        this.picture    =     $(picture);
-        this.relative   =     $(this.options.relative).setStyles({cursor:'move'}).scrollTo(-this.left,-this.top);
+        this.picture    =     document.id(picture);
+        this.relative   =     document.id(this.options.relative).setStyles({cursor:'move'}).scrollTo(-this.left,-this.top);
         this.parent(this.relative);
-		this.__construct();
+        this.__construct();
     },
-	__construct	:    function(){
-		if(Browser.Engine.trident) document.ondragstart = function (){return false;}; // IE fix
-	},
-    coordinates	: function(){
-		this.getCoordinates	=	this.relative.getScroll();
-		this.getSize		=	this.relative.getSize();
-        this.width  		=   this.relative.getSize().x.toInt();
-        this.height  		=   this.relative.getSize().y.toInt();
-        this.left  			=   -this.getCoordinates.x.toInt();
-        this.top    		=   -this.getCoordinates.y.toInt();
+    __construct :    function(){
+        if(Browser.name.ie) document.ondragstart = function (){return false;}; // IE fix
     },
-	complete    :    function(){
-		this.coordinates();
-		this.fireEvent("done",[this.top,this.left,this.width,this.height]);
-	}
+    coordinates : function(){
+        this.getCoordinates =   this.relative.getScroll();
+        this.getSize        =   this.relative.getSize();
+        this.width          =   this.relative.getSize().x.toInt();
+        this.height         =   this.relative.getSize().y.toInt();
+        this.left           =   -this.getCoordinates.x.toInt();
+        this.top            =   -this.getCoordinates.y.toInt();
+    },
+    complete    :    function(){
+        this.coordinates();
+        this.fireEvent("done",[this.top,this.left,this.width,this.height]);
+    }
 });
